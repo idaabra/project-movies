1f6901f5d0cdca4f54c81ddf473fb770c6253a5c
Ida Abrahamsson <idaabrahamsson@hotmail.com>
2020-04-04 00:48:19 +0200

    started with styling

--- code/src/pages/MovieDescription.js
+++ code/src/pages/MovieDescription.js
@@ -4,29 +4,33 @@ import { NavLink, useParams } from 'react-router-dom'
 
 export const MovieDescription = () => {
   const { movieId } = useParams()
-  const [movies, setMovies] = useState([])
+  const [movie, setMovie] = useState([])
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=34d4221efb441d76ee99051efaa0994f&language=en-US`)
       .then((res) => res.json())
       .then((json) => {
-        setMovies(json)
+        setMovie(json)
       })
   }, [movieId])
 
+  if (!movie) {
+    return <></>
+  }
+
   return (
-    <section style={{ backgroundImage: `url("http://image.tmdb.org/t/p/w1280${movies.backdrop_path}")` }}>
+    <section style={{ backgroundImage: `url("http://image.tmdb.org/t/p/w1280${movie.backdrop_path}")` }}>
       <NavLink to="/" exact> <span role="img" aria-label="arrow">&#9666;</span> <p>Back</p> </NavLink>
       <div>
-        <img src={`http://image.tmdb.org/t/p/w342${movies.poster_path}`} alt={`${movies.title} cover`} />
+        <img src={`http://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={`${movie.title} cover`} />
       </div>
       <div>
-        <h1> {movies.title} </h1>
-        <p> {movies.overview} </p>
-        <h2> {movies.tagline} </h2>
+        <h1> {movie.title} </h1>
+        <p> {movie.overview} </p>
+        <h2> {movie.tagline} </h2>
       </div>
       <div>
-        <h3><span role="img" aria-label="star">&#11088;</span> {movies.vote_average}/10 based on {movies.vote_count} votes</h3>
+        <h3><span role="img" aria-label="star">&#11088;</span> {movie.vote_average}/10 based on {movie.vote_count} votes</h3>
       </div>
 
     </section>
--- code/src/pages/MovieList.js
+++ code/src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import './StyleList.css'
 
 
 
@@ -15,13 +16,15 @@ export const MovieList = () => {
   }, [])
 
   return (
-    <div>
+    <div className="movie-list">
       {movies.map((movie) => (
-        <div key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>
+        <div key={movie.id} >
+          <Link className="movie-goto" to={`/movies/${movie.id}`}>
             <img src={` https://image.tmdb.org/t/p/w500${movie.poster_path} `} alt={movie.title} />
-            <h2>{movie.title}</h2>
-            <h3> <span role="img" aria-label="star">&#11088;</span> {movie.vote_average}/10</h3>
+            <div className="chooseMovieText">
+              <h2>{movie.title}</h2>
+              <h3> <span role="img" aria-label="star">&#11088;</span> {movie.vote_average}/10</h3>
+            </div>
           </Link>
         </div>
 
